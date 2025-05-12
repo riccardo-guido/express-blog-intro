1e8b76059fb484557c38c27aa2bd0bbf21b94570
riccardo-guido <guidoriccardo02@gmail.com>
2025-05-12 17:09:19 +0200

    json-object-/-new-path-/-static-assests-/-test-postman

--- app.js
+++ app.js
@@ -2,8 +2,42 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Server del mio blog");
+app.use(express.static("public"));
+
+app.get("/bacheca", (req, res) => {
+  //   res.send("Server del mio blog");
+  res.type("JSON").send([
+    {
+      title: "post 1",
+      content: "dolce 1",
+      img: "./images/1.jpeg",
+      tags: ["dolci", "frutta", "zucccheri"],
+    },
+    {
+      title: "post 2",
+      content: "dolce 2",
+      img: "./images/2.jpeg",
+      tags: ["dolci", "frutta", "zucccheri"],
+    },
+    {
+      title: "post 3",
+      content: "dolce 3",
+      img: "./images/3.jpeg",
+      tags: ["dolci", "frutta", "zucccheri"],
+    },
+    {
+      title: "post 4",
+      content: "dolce 4",
+      img: "./images/4.jpeg",
+      tags: ["dolci", "frutta", "zucccheri"],
+    },
+    {
+      title: "post 5",
+      content: "dolce 5",
+      img: "./images/5.jpeg",
+      tags: ["dolci", "frutta", "zucccheri"],
+    },
+  ]);
 });
 app.listen(port, () => {
   console.log(`Il server è in ascolto sulla porta  ${port}`);
